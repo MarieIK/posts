1d8a840c2dd7b393f82e21937221dab3a93c0f63
Maria Kuznetsova <mikuznetsova@list.ru>
2022-06-21 22:11:17 +0300

    Updated

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -3,50 +3,29 @@ import java.time.LocalDateTime
 data class Post(
     val id: Int = 0,
     val ownerId: Long = 0,
-    val fromId: Long = 0,
-    val createdBy: Int = 0,
     val date: LocalDateTime = LocalDateTime.now(),
     val text: String? = null,
-    val replyOwnerId: Long = 1,
-    val replyPostId: Long = 1,
-    val friendsOnly: Boolean = true,
     val comments: Comments? = null,
     val copyright: Copyright? = null,
     val likes: Likes = Likes(),
     val reposts: Reposts? = null,
     val views: Views = Views(),
     val postType: PostType,
-    val signerId: Long = 0,
-    val canPin: Boolean = true,
-    val canDelete: Boolean = true,
-    val canEdit: Boolean = true,
-    val isPinned: Boolean = true,
-    val markedAsAds: Boolean = true,
-    val isFavourite: Boolean = true,
-    val donut: Donut,
-    val postponedId: Int = 1
 )
 
 data class Comments(
     var count: Long = 5,
     val canEdit: Boolean = true,
-    val groupsCanPost: Boolean = true,
-    val canClose: Boolean = true,
-    val canOpen: Boolean = true
 )
 
 data class Copyright(
     val id: Long = 0,
     val link: String = "",
-    val name: String = "",
-    val type: String = ""
 )
 
 data class Likes(
     var count: Long = 90,
     val userLikes: Boolean = true,
-    val canLike: Boolean = true,
-    val canPublish: Boolean = true
 )
 
 data class Reposts(
@@ -57,24 +36,7 @@ data class Reposts(
 data class Views(var count: Long = 150)
 
 enum class PostType {
-    POST,
     COPY,
-    REPLY,
     POSTPONE,
     SUGGEST
-}
-
-data class Donut(
-    val isDonut: Boolean = true,
-    var paidDuration: Long = 10,
-    val placeholder: Placeholder = Placeholder(),
-    val canPublishFreeCopy: Boolean = true,
-    var editMode: DonutEditMode
-)
-
-data class Placeholder(val someText: String = "Some placeholder")
-
-enum class DonutEditMode {
-    ALL,
-    DURATION
 }
