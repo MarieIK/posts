8cea77486f704c574955a4df761a3d48460d8e25
Maria Kuznetsova <mikuznetsova@list.ru>
2022-06-21 20:46:37 +0300

    Nullable

--- src/main/kotlin/Donut.kt
+++ src/main/kotlin/Donut.kt
@@ -1,7 +1,7 @@
-data class Donut(
-    val isDonut: Boolean = true,
-    var paidDuration: Long = 10,
-    val placeholder: Placeholder = Placeholder(),
-    val canPublishFreeCopy: Boolean = true,
-    var editMode: DonutEditMode
-)
+//data class Donut(
+//    val isDonut: Boolean = true,
+//    var paidDuration: Long = 10,
+//    val placeholder: Placeholder = Placeholder(),
+//    val canPublishFreeCopy: Boolean = true,
+//    var editMode: DonutEditMode
+//)
--- src/main/kotlin/Reposts.kt
+++ src/main/kotlin/Reposts.kt
@@ -1,4 +1,4 @@
-data class Reposts(
-    var count: Long = 100,
-    val userReposted: Boolean = true
-)
+//data class Reposts(
+//    var count: Long = 100,
+//    val userReposted: Boolean = true
+//)
